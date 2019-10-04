e18a568e4eb84ca2fbef855b50addfd4bb3b2323
Richard Sunden <djrap1dtecno@gmail.com>
2019-10-04 19:51:38 -0400

    Init - 6.2 Complete

--- ConvertIt/ViewController.swift
+++ ConvertIt/ViewController.swift
@@ -33,15 +33,12 @@ extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
-    
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return formulaArray.count
     }
-    
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return formulaArray[row]
     }
-    
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         fromUnitsLabel.text = formulaArray[row]
     }
